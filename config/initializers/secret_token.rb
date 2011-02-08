07c315e651ea33e271c49edc5cfa789d11ef499b
Daz Oakley <do2@sanger.ac.uk>
2011-02-08 09:04:48 +0000

    Initial commit of Rails skeleton

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+EummcrOrdering::Application.config.secret_token = '2edd58206ad64a3962abf1aff3641b0dbad025bd14ed5a320109d2dd404188814880bbfbe1a1d39fd8131fdf4df9c481678127fdb2122fd83cac7fbb3808a69b'
