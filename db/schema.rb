fc264b6f09a08a3a4e25bbe25ca3bdbe3bb87f1a
Daz Oakley <do2@sanger.ac.uk>
2011-02-08 13:37:43 +0000

    Removed the clone model and table - it's not required...

--- db/schema.rb
+++ db/schema.rb
@@ -12,12 +12,6 @@
 
 ActiveRecord::Schema.define(:version => 20110208132505) do
 
-  create_table "clones", :force => true do |t|
-    t.string   "name",       :null => false
-    t.datetime "created_at"
-    t.datetime "updated_at"
-  end
-
   create_table "genes", :force => true do |t|
     t.string   "symbol",     :null => false
     t.datetime "created_at"
