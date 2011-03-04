234f625b1a3d95dae7aeff9c87f1454f1dfc8c7d
Daz Oakley <do2@sanger.ac.uk>
2011-03-04 10:21:09 +0000

    Updated the functional tests so that they will pass

--- test/functional/settings_controller_test.rb
+++ test/functional/settings_controller_test.rb
@@ -1,9 +1,16 @@
 require 'test_helper'
 
 class SettingsControllerTest < ActionController::TestCase
-  test "should get index" do
+  test "should NOT get index" do
     get :index
+    assert_response 302
+  end
+  
+  test "should get JSON settings" do
+    get :'index', { :format => 'json' }
     assert_response :success
+    resp = JSON.parse(@response.body)
+    assert_not_nil(resp)
   end
 
 end
