056aa654bc1e325ecfbebf0c1b87fcd973fec382
Daz Oakley <do2@sanger.ac.uk>
2011-03-04 10:22:51 +0000

    Updated Rails to 3.0.5

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -4,4 +4,4 @@
 # If you change this key, all old signed cookies will become invalid!
 # Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
-EummcrOrdering::Application.config.secret_token = '2edd58206ad64a3962abf1aff3641b0dbad025bd14ed5a320109d2dd404188814880bbfbe1a1d39fd8131fdf4df9c481678127fdb2122fd83cac7fbb3808a69b'
+EummcrOrdering::Application.config.secret_token = '9c8a56d1f464520e57a9ac5aebb8d5a1c7123b98819688da1c5d6090507e0976e8574d4fd13027a034ffebd24f1aa131dd2671e2fd3a60ba0843427d22b29d58'
